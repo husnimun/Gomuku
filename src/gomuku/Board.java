82a455c56966b496e93023511e99923b57397c9c
Luqman A. Siswanto <l.arifin.siswanto@gmail.com>
2015-11-30 13:55:25 +0700

    Add board validation

--- src/gomuku/Board.java
+++ src/gomuku/Board.java
@@ -8,6 +8,8 @@ public class Board {
     private int boardWidth = 20;
     private int boardHeight = 20;
     private int[][] board = new int[boardWidth][boardHeight];
+    private static int dx[] = {-1, -1, 0, 1, -1, 0, 1, 1};
+    private static int dy[] = {0, 1, 1, 1, -1, -1, -1, 0};
     
     public Board() {
         for (int i = 0; i < boardWidth; i++) {
@@ -17,10 +19,21 @@ public class Board {
         }
     }
     
-    public void add(int x, int y, int el) {
+    /**
+     * 
+     * @param x coordinate-x
+     * @param y coordinate-y
+     * @param el player
+     * @return true if 'el' winning when put token
+     * false otherwise
+     */
+    public boolean add(int x, int y, int el) {
         if (isEmpty(x, y)) {
             board[x][y] = el;
+        } else {
+            // do some action when board revisited
         }
+        return isWin(x, y, el);
     }
     
     public void print() {
@@ -35,5 +48,29 @@ public class Board {
     
     public boolean isEmpty(int x, int y) {
         return board[x][y] == -1;
+    }
+    
+    private boolean isWin(int x, int y, int el) {
+        assert board[x][y] == el;
+        int[] isi = new int[8];
+        for(int i = 0; i < 8; i++) {
+          int tx = x + dx[i];
+          int ty = y + dy[i];
+          while(isValid(tx, ty) && board[tx][ty] == el) {
+              isi[i]++;
+              tx += dx[i];
+              ty += dy[i];
+          }
+        }
+        for(int i = 0; i < 8; i++) {
+            if(isi[i] + isi[7 - i] >= 4) {
+                return true; 
+            }
+        }
+        return false;
+    }
+    
+    private boolean isValid(int x, int y) {
+        return 0 <= x && x <= boardWidth && 0 <= y && y <= boardHeight;
     }
 }
